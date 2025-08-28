9ccbd1aff242a7c2a85f9d887d9d36292c99abdd
Yasin <yasinpeker43@gmail.com>
2025-08-28 09:14:47 +0300

    feat(migrations) add migrations and update database

--- obj/Debug/net8.0/PanelProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/PanelProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PanelProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7e177f71c18e599504b078c13443db5b8f43208")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf1e1846e8d124242381162b389048ddd9c614ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("PanelProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PanelProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
