ac4826dbd1db3ddc0d5be03881de28e4201f3165
Yasin <yasinpeker43@gmail.com>
2025-08-31 10:31:47 +0300

    feat(TagHelper) fix typo

--- TagHelpers/RoleUserTagHelper.cs
+++ TagHelpers/RoleUserTagHelper.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using PanelProject.Models;
@@ -17,7 +18,39 @@ namespace PanelProject.TagHelpers
             _roleManager = roleManager;
             _userManager = userManager;
         }
-        
-        
+
+        [HtmlAttributeName("asp-role-users")]
+
+        public string RoleId { get; set; } = null!;
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var userNames = new List<string>();
+            var role = await _roleManager.FindByIdAsync(RoleId);
+
+            if (role != null && role.Name != null)
+            {
+                foreach (var user in _userManager.Users)
+                {
+                    if (await _userManager.IsInRoleAsync(user, role.Name))
+                    {
+                        userNames.Add(user.UserName ?? "");
+                    }
+                }
+                output.Content.SetHtmlContent(userNames.Count == 0 ? "There's no user." : setHtml(userNames));
+            }
+
+
+        }
+        private string setHtml(List<string> userNames) {
+            var html = "<ul>";
+            foreach (var item in userNames)
+            {
+                html += "<li>" + item + "</li>";
+            }
+            html += "</ul>";
+            return html;
+
+        }
     }
 }
