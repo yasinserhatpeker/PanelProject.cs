6e0b09869d25e03d9c9472c4d2d19cc056e0315a
Yasin <yasinpeker43@gmail.com>
2025-08-31 11:01:49 +0300

    feat(roleController) add role edit httpost and httpget

--- Controllers/RolesController.cs
+++ Controllers/RolesController.cs
@@ -9,9 +9,12 @@ namespace PanelProject.Controllers
     {
         private readonly RoleManager<AppRole> _roleManager;
 
-        public RolesController(RoleManager<AppRole> RoleManager)
+        private readonly UserManager<AppUser> _userManager;
+
+        public RolesController(RoleManager<AppRole> roleManager, UserManager<AppUser> userManager)
         {
-            _roleManager = RoleManager;
+            _roleManager = roleManager;
+            _userManager = userManager;
         }
 
 
@@ -43,7 +46,44 @@ namespace PanelProject.Controllers
             }
             return View(model);
         }
-        
-        
+
+        public async Task<IActionResult> Edit(string Id)
+        {
+            var role = await _roleManager.FindByIdAsync(Id);
+            if (role != null && role.Name != null)
+            {
+                ViewBag.Users = await _userManager.GetUsersInRoleAsync(role.Name);
+                return View(role);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(AppRole model)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = await _roleManager.FindByIdAsync(model.Id);
+
+                if (role != null)
+                {
+                    role.Name = model.Name;
+                    var result = await _roleManager.UpdateAsync(role);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    foreach (var err in result.Errors)
+                    {
+                        ModelState.AddModelError("", err.Description);
+                    }
+                    ViewBag.Users = await _userManager.GetUsersInRoleAsync(role.Name);
+                }
+
+            }
+            return View(model);
+             
+        }
     }
 }
--- obj/Debug/net8.0/PanelProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/PanelProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PanelProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac4826dbd1db3ddc0d5be03881de28e4201f3165")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+529d0c74b521a7d4ccf4bcafc3dea289f26f9259")]
 [assembly: System.Reflection.AssemblyProductAttribute("PanelProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PanelProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
